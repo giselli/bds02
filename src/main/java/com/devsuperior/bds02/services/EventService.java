57278ba23515f8387be2cec0023532f7d6711292
Giselli Hara <gi@hara.ninja>
2021-06-23 14:34:10 -0300

    testes ok

--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -1,8 +1,8 @@
 package com.devsuperior.bds02.services;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,28 +10,27 @@ import com.devsuperior.bds02.dto.EventDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.entities.Event;
 import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class EventService {
-	
-	@Autowired	
+
+	@Autowired
 	private EventRepository repository;
-	
-	@Transactional(readOnly=true)
-	public Page<EventDTO> findAll(Pageable pageable) {
-		Page<Event> page = repository.findAll(pageable);
-		return page.map(x -> new EventDTO(x));
-	}
 
 	@Transactional
-	public EventDTO insert(EventDTO dto) {
-				Event entity = new Event();
-				entity.setName(dto.getName());
-				entity.setDate(dto.getDate());
-				entity.setUrl(dto.getUrl());
-				entity.setCity(new City(dto.getCityId(), null));
-				entity = repository.save(entity);
-				return new EventDTO(entity);
-			}
-}
+	public EventDTO update(Long id, EventDTO dto) {
+		try {
+			Event event = repository.getOne(id);
+			event.setCity(new City(dto.getCityId(), null));
+			event.setDate(dto.getDate());
+			event.setName(dto.getName());
+			event.setUrl(dto.getUrl());
+			event = repository.save(event);
+			return new EventDTO(event);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("ID not found");
+		}
+	}
 
+}
--- src/test/java/com/devsuperior/bds02/controllers/EventControllerIT.java
+++ src/test/java/com/devsuperior/bds02/controllers/EventControllerIT.java
@@ -68,4 +68,5 @@ public class EventControllerIT {
 		
 		result.andExpect(status().isNotFound());
 	}
+	
 }
